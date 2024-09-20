d48f8755dacfff7e70226166c0c7bb202dbe51bd
Manas Narkar <narkarmanas854@gmail.com>
2024-09-20 08:37:24 -0400

    First Commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+print("Welcome to calculator!")
+num1 = int(input("Enter num #1 "))
+num2 = int(input ("Enter num #2 "))
+operation = input("Add, subtract, multiply or divide?")
+if operation == "add":
+    print(num1 + num2)
+if operation == "subtract":
+    print(num1 - num2)
+if operation == "multiply":
+    print(num1 * num2)
+if operation == "divide":
+    print(num1 / num2)
+
+
