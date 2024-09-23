78a567e6eabd6b5b76613a10888e42a160c204cc
bigkangaroo123 <narkarmanas854@gmail.com>
2024-09-23 09:40:32 -0400

    Create tiles.py

---
+++ tiles.py
@@ -0,0 +1,9 @@
+import math
+
+tiles = int(input("how many tiles?: "))
+
+side_length = math.sqrt(tiles)
+side_length = math.floor(tiles)
+
+if side_length** <= tiles**:
+    print("The largest square has side length (side_length)")
