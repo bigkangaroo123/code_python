db3eca5d9a4fa58b9c7b07ec781101f662fb2669
bigkangaroo123 <narkarmanas854@gmail.com>
2024-09-24 08:39:19 -0400

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -2,8 +2,17 @@ import math
 
 tiles = int(input("how many tiles?: "))
 
-side_length = math.sqrt(tiles)
-side_length = math.floor(tiles)
+side_length = math.floor(math.sqrt(tiles))
 
-if side_length** <= tiles**:
-    print("The largest square has side length (side_length)")
+#if side_length^2 = tiles, return side_length
+#if not, check every number behind side_length and square it to see if that number is less than tiles, stop when found
+
+if side_length*side_length == tiles:
+    print(f"The largest square has the side length {side_length}")
+else:
+    i = side_length
+    while i <= side_length and i >= 0:
+        if i*i <= tiles:
+            print(f"The largest square has the side length {i}")
+            break
+        i = i - 1
