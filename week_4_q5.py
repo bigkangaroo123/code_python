3d7e85c14484dd1c146c491eb2c7c94b2aee997a
bigkangaroo123 <narkarmanas854@gmail.com>
2024-10-11 08:45:33 -0400

    Update week_4_q5.py

--- week_4_q5.py
+++ week_4_q5.py
@@ -20,25 +20,23 @@ def isAbundant(data):
                     factors.clear()
     return f" {abundant_even} are even abundant and {abundant_odd} are odd abundant"
 
-'''
-def abundant_or_deficient_or_perfect(num):
-    factors = []
-    for i in range(1, num):
-        if num % i == 0:
-            factors.append(i)
-    #end
-    sum_of_factors = sum(factors)
-    if sum_of_factors < num:
-        return "deficient"
-    elif sum_of_factors == num:
-        return "PERFECT NUMBE!!11!1"
-    else:
-        return "abundant"
-#end
-num = int(input("Enter a positive integer: "))
-choice = abundant_or_deficient_or_perfect(num)
-print(choice)
-'''
+# def abundant_or_deficient_or_perfect(num):
+#     factors = []
+#     for i in range(1, num):
+#         if num % i == 0:
+#             factors.append(i)
+#     #end
+#     sum_of_factors = sum(factors)
+#     if sum_of_factors < num:
+#         return "deficient"
+#     elif sum_of_factors == num:
+#         return "PERFECT NUMBE!!11!1"
+#     else:
+#         return "abundant"
+# #end
+# num = int(input("Enter a positive integer: "))
+# choice = abundant_or_deficient_or_perfect(num)
+# print(choice)
 
 print("Enter 10 positive integers: ")
 data = []
