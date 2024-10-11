53fe76e966d563ffc7d2c598d3fcebf43ffa516f
bigkangaroo123 <narkarmanas854@gmail.com>
2024-10-11 08:38:55 -0400

    Update week_4_q3.py

--- week_4_q3.py
+++ week_4_q3.py
@@ -1,13 +1,24 @@
-data = [7, 21, 5, 19, 6, 2]
-k = len(data) - 1
-a = 1
-b = 0
-while k >= 0:
-    if data[k] % 2 < 1: #checks if data[k] is even
-        a = a * data[k] #a = every even value in data, multiplied
-    else:
-        b += 1 #b = number of odd numbers in data
-    k -= 1
+def odd_and_even(array):
+    k = len(data) - 1
+    a = 1
+    b = 0
+    while k >= 0:
+        if data[k] % 2 == 0: #checks if data[k] is even
+            a = a * data[k] #a = every even value in data, multiplied
+        else:
+            b += 1 #b = number of odd numbers in data
+        k -= 1
+    temp = [a, b]
+    return temp
+        
+print("Enter 10 integer values: ")
+data = []
+for i in range(10):
+    num = int(input())
+    data.append(num)
 
+final = odd_and_even(data)
+a = final[0]
+b = final[1]
 print(a)
 print(b)
