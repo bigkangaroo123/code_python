b67d8912224d1ade9719786b3f8e1666ff81f4fe
bigkangaroo123 <narkarmanas854@gmail.com>
2024-10-15 08:45:19 -0400

    Update palindrome_checker.py

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,6 +1,12 @@
 def palindrome_checker(word) -> bool: #its like hinting what the function returns - doesnt mean anything tho
-    word = word.lower()
-    return word == word[::-1] #return true or false
+    if not word:
+        return True
+    elif len(word) == 1:
+        return True
+    else:   
+        word = word.lower()
+        return word == word[::-1] # :: sorts a string based on the argument, ex - -2 would've been switching every 2 indexes of the word
+        #return true or false
 
 a = input("Enter a word: ")
 is_palindrome = palindrome_checker(a)
