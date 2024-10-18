cb3fa0a857bf62185c266ad35b74ce803959f9c4
bigkangaroo123 <narkarmanas854@gmail.com>
2024-10-17 22:05:24 -0400

    Update quadrants.py

--- quadrants.py
+++ quadrants.py
@@ -9,3 +9,13 @@ elif x < 0 and y < 0:
     print("Quadrant 3")
 elif x > 0 and y < 0:
     print("Quadrant 4")
+elif x > 0 and y == 0:
+    print("On the X axis, between Q1 and Q4")
+elif x < 0 and y == 0:
+    print("On the X axis, between Q2 and Q3")
+elif x == 0 and y > 0:
+    print("On the Y axis, between Q1 and Q2")
+elif x == 0 and y < 0:
+    print("On the Y axis, between Q3 and Q4")
+else:
+    print("Origin")
