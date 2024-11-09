e3aad9e8b0d2d3903dc2520194b5f27df7a68037
bigkangaroo <narkarmanas854@gmail.com>
2024-11-09 09:59:40 -0500

    again

--- CCC_S1.py
+++ CCC_S1.py
@@ -20,13 +20,6 @@ colours = ["pink", "green", "red", "orange"]
 all_combinations = []
 min_cost = None
 
-sum = 0
-for p in range(target // pink + 1):
-    for g in range(target // green + 1)):
-        for r in range(target // red + 1):
-            for o in range(target // orange + 1):
-                if p*pink + g*green + r*red + o*orange == money:
-                    all_combinations.append((p, g, r, o))
-                    if P+g+r+o < min_
+
                 
     
