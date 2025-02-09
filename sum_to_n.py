7247f683f47e315296ba92a05a4138fce0199cdb
bigkangaroo <narkarmanas854@gmail.com>
2025-02-09 14:44:11 -0500

    FUCK THIS SWIPE PROBLEM

---
+++ sum_to_n.py
@@ -0,0 +1,18 @@
+# def sum1(n, val=0):
+#     #O(n)
+#     for i in range(1, n+1):
+#         val += i
+#     return val
+
+# def sum2(n):
+#     #O(1)
+#     return (n*(n+1)) // 2
+
+# n = int(input())
+# print(sum2(n))
+# print(sum1(n))
+
+n = int(input())
+while n:
+    t = int(input())
+    n -= 1
